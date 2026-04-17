6bdd882bf6c902af399ff6a8ab50256038f5fb1d
Albert <agamerawesome@gmail.com>
2026-04-18 00:04:13 +0300

    Update shard.ts
    meep

--- src/data/shard.ts
+++ src/data/shard.ts
@@ -121,7 +121,7 @@ const confirmedObservations: ConfirmedObservation[] = [
   { date: '2025-10-17', realmIndex: 4, map: 'vault.jelly' }, // Memory: Jellyfish [1], Candles: 3.5
   { date: '2025-10-19', realmIndex: 1, map: 'forest.tree' }, // IDK
   { date: '2025-10-21', realmIndex: 4, map: 'vault.starlight' }, // Memory: None [Black Shard]
-  { date: '2026-04-19', realmIndex: 4, map: 'vault.starlight' }, // IDK
+  { date: '2026-04-17', realmIndex: 4, map: 'vault.starlight' }, // IDK
 ];
 
 function getNetEaseRealmRotation(date: DateTime): { realmIndex: number; map: Areas } {
